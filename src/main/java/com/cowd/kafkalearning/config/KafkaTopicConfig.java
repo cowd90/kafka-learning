ac0946fe866aaeb6dc8f32095b19c254362793a0
cowd90 <tienn.dungg.980@gmail.com>
2024-06-01 10:06:29 +0700

    config kafka producer, consumer and topic

--- src/main/java/com/cowd/kafkalearning/config/KafkaTopicConfig.java
+++ src/main/java/com/cowd/kafkalearning/config/KafkaTopicConfig.java
@@ -1,2 +1,18 @@
-package com.cowd.kafkalearning.config;public class KafkaTopicConfig {
+package com.cowd.kafkalearning.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic myTopic() {
+        return TopicBuilder
+                .name("cowd_topic")
+                .build();
+    }
+
 }
